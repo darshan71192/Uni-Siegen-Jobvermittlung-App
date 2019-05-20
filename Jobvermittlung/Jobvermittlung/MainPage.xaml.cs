2f38b674d7ef19354138da962042c7cda10d10a5
dash <darshan71192@gmail.com>
2019-05-21 01:16:43 +0200

    init

---
+++ Jobvermittlung/Jobvermittlung/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Xamarin.Forms;
+using HtmlAgilityPack;
+
+namespace Jobvermittlung
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public IList<JobListing> ListOfAllJobs { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //ListOfAllJobs = new List<JobListing>();
+            GetAllJobs();
+
+            BindingContext = this;
+        }
+
+        private void GetAllJobs()
+        {
+            ListOfAllJobs = new List<JobListing>();
+
+            // TODO web scrapping and populating data
+            var url = "https://www.jobvermittlung.uni-siegen.de/studierende/stellenangebote.xml";
+
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            var nodes = doc.DocumentNode.Descendants("div")
+                                        .Where(x => x.Attributes["class"] != null &&
+                                                    x.Attributes["class"].Value == "block" && 
+                                                    x.Attributes["id"] != null && 
+                                                    x.Attributes["id"].Value != "");
+            foreach (var node in nodes)
+            {
+                var id = node.SelectSingleNode("div/strong/a").InnerText as string;
+                var title = node.SelectSingleNode("h2/a").InnerText;
+                var details = node.SelectSingleNode("p").InnerText;
+                var time = node.SelectSingleNode("ul/li[1]").InnerText;
+                var lohn = node.SelectSingleNode("ul/li[2]").InnerText;
+                var ort = node.SelectSingleNode("ul/li[3]").InnerText;
+                var sprache = "N/A";
+                if (node.SelectSingleNode("ul/li[4]") != null)
+                {
+                    sprache = node.SelectSingleNode("ul/li[4]").InnerText;
+                }                
+                ListOfAllJobs.Add(new JobListing(id.Trim(), title, details, time, lohn, ort, sprache));
+            }
+        }        
+    }
+}
---
+++ Jobvermittlung/Jobvermittlung/Models/JobListing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jobvermittlung
+{
+    public class JobListing
+    {
+        public string Id { get; private set; }
+        public string Title { get; private set; }
+        public string Details { get; private set; }
+        public string ArbeitZeit { get; private set; }
+        public string Lohn { get; private set; }
+        public string Ort { get; private set; }
+        public string Sprachniveau { get; private set; }        
+
+        public JobListing(string id, string title, string details, string arbeitZeit, string lohn, string ort, string sprachniveau)
+        {
+            Id = id;
+            Title = title;
+            Details = details;
+            ArbeitZeit = arbeitZeit;
+            Lohn = lohn;
+            Ort = ort;
+            Sprachniveau = sprachniveau;
+        }
+    }
+}
